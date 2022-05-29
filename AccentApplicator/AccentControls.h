77e931d637bfb87c773c8fa0745ba3d2ae06c14f
rounk-ctrl <70931017+rounk-ctrl@users.noreply.github.com>
2022-05-29 10:47:00 +0400

    new file for values

---
+++ AccentApplicator/AccentControls.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "framework.h"
+#include "AccentColorHelper.h"
+
+void Accent_Button(int* r, int* g, int* b, int* a);
+void Accent_CheckBox(int* r, int* g, int* b, int* a);
+void Accent_Tab(int* r, int* g, int* b, int* a);
+void Accent_Trackbar(int* r, int* g, int* b, int* a);
+void Accent_Spin(int* r, int* g, int* b, int* a);
